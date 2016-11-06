45c216f1f7cbb541c5dfdc5a8d4539f139dd85ef
Kaboose <kevintjet@hotmail.com>
2016-11-06 21:56:27 +0100

    Had to reset project due to messed up dependencies. Clean LibGDX installs

---
+++ projects/libgdx-clean-setup/html/src/be/howest/twentytwo/parametergame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package be.howest.twentytwo.parametergame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import be.howest.twentytwo.parametergame.ParameterGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ParameterGame();
+        }
+}
---
+++ projects/libgdx/desktop/src/be/howest/twentytwo/parametergame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package be.howest.twentytwo.parametergame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import be.howest.twentytwo.parametergame.ParameterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ParameterGame(), config);
+	}
+}
