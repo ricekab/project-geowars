b19d2a0f0bc2f8fafc3deab28c204196879c8f3c
Kaboose <kevintjet@hotmail.com>
2016-11-08 08:31:23 +0100

    Added Ashley's Engine instance.

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/ParameterGame.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/ParameterGame.java
@@ -3,6 +3,7 @@ package be.howest.twentytwo.parametergame;
 import be.howest.twentytwo.parametergame.platform.IPlatformSpecificAPI;
 import be.howest.twentytwo.parametergame.screen.MenuScreen;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,14 +13,17 @@ import com.badlogic.gdx.utils.Logger;
 public class ParameterGame extends Game {
 	public static final boolean DEBUG_ENABLED = true;
 	
+	// TODO: Move this stuff into a service container
 	public AssetManager assetMgr;
 	public SpriteBatch batch;
 	public Logger logger;
 	public IPlatformSpecificAPI platformAPI;
+	public Engine ecsEngine;
 	// public final I18NBundle messages;
 	
 	public ParameterGame(IPlatformSpecificAPI platform){
 		this.platformAPI = platform;
+		this.ecsEngine = new Engine();
 	}
 	
 	@Override
