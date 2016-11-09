18c08c06fd607ee85715329e90a0bf73df18242a
Nick Dewitte <Nick Dewitte>
2016-11-09 10:42:03 +0100

    quick push to have temporary data on ships

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/db/IDataService.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/db/IDataService.java
@@ -1,5 +1,6 @@
 package be.howest.twentytwo.parametergame.service.db;
 
 public interface IDataService {
+	//This is the general class, that communicates with different classes, such as MySQLDataService, T-SQLDataService,....
 
 }
