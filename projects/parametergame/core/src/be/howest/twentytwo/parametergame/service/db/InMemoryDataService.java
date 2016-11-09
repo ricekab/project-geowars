3ddeeb26e8fbfaf6ab056abd8694fc8429052a74
Nick Dewitte <Nick Dewitte>
2016-11-09 14:09:58 +0100

    Wrote tests, added JSON dependency

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/db/InMemoryDataService.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/db/InMemoryDataService.java
@@ -6,79 +6,95 @@ import java.util.Map;
 public class InMemoryDataService implements IDataService {
 //for the time being, this will have hard-coded data, and is later DELETED
 	//	data management josb: faking access to MySQL data and return the appropriate data
-	//	
+	//	TODO make abstract class to manage DB systems?
 	
 	private Map<String, String> data = new HashMap<String, String>();
 	
 	
 	public Map<String, String> getEnemy(String name) {
-		data = null;
+		data.clear();
+		data.put("error", "none");
+		name = name.toLowerCase();
 		switch(name) {
-			case "X-Wing":
+			case "x-wing":
 				data.put("speed mult", "100");
 				data.put("health", "3");
 				data.put("turn rate", "5");
-				data.put("behaviour", "{spawn:run}");
+				data.put("behaviour", "{\"spawn\":\"run\"}");
 				break;
-			case "Tie-Fighter":
+			case "tie-fighter":
 				data.put("speed mult", "90");
 				data.put("health", "2");
 				data.put("turn rate", "1");
-				data.put("behaviour", "{spawn:run,hit:suicide");
+				data.put("behaviour", "{\"spawn\":\"run\",\"hit\":\"suicide\"}");
 				break;
-			case "death-star":
-				data.put("speed mult", "0");
+			case "deathstar":
+				data.put("speed mult", "10");
 				data.put("health", "1");
 				data.put("turn rate", "10");
 				data.put("behaviour", "stationary");
+				break;
+			default:
+				data.put("error", "404 not found");
 				break;
 		}
 		return data;
 	}
 	
 	public Map<String, String> getShip(String name) {
-		data = null;
+		data.clear();
+		data.put("error", "none");
+		name = name.toLowerCase();
 		switch(name) {
-		case "Starship Enterprice":
+		case "starship enterprice":
 			data.put("level", "15");
 			data.put("exp", "7500");
 			data.put("type", "juggernaut");
 			data.put("speed mult", "50");
-			data.put("drones", "{slot1:USS Zhukov, slot2:USS Farragut, slot3:USS Valiant}");
+			data.put("drones", "{\"slot1\":\"uqq zhukov\", \"slot2\":\"uss farragut\", \"slot3\":\"uss valiant\"}");
 			break;
-		case "Millenium Falcon":
+		case "millenium falcon":
 			data.put("level", "1");
 			data.put("exp", "0");
 			data.put("type", "recon");
 			data.put("speed mult", "175");
-			data.put("drones", "{slot1:Y-Wing, slot2:null, slot3:null}");
+			data.put("drones", "{\"slot1\":\"y-wing\", \"slot2\":\"null\", \"slot3\":\"null\"}");
+			break;
+		default:
+			data.put("error", "404 not found");
+			data.put("name", name);
 			break;
 		}
 		return data;
 	}
 	
 	public Map<String, String> getDrone(String name) {
-		data = null;
+		data.clear();
+		data.put("error", "none");
+		name = name.toLowerCase();
 		switch(name) {
-		case "USS zhukov":
+		case "uss zhukov":
 			data.put("level", "1");
 			data.put("exp", "57");
 			data.put("effect", "collect");
 			break;
-		case "USS Farragut":
+		case "uss farragut":
 			data.put("level", "3");
 			data.put("exp", "517");
 			data.put("effect", "speed mult");
 			break;
-		case "USS Valiant":
+		case "uss valiant":
 			data.put("level", "10");
 			data.put("exp", "9001");
 			data.put("effect", "attack speed");
 			break;
-		case "Y-Wing":
+		case "y-wing":
 			data.put("level", "30");
 			data.put("exp", "0");
 			data.put("effect", "bullet spread");
+			break;
+		default:
+			data.put("error", "404 not found");
 			break;
 		}
 		return data;
