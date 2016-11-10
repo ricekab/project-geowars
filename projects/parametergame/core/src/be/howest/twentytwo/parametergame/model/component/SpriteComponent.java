9d8722422cb395fde990f18aa52f740fd99ac731
Kaboose <kevintjet@hotmail.com>
2016-11-10 08:05:07 +0100

    Moved ComponentMapper into Component classes.

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/model/component/SpriteComponent.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/model/component/SpriteComponent.java
@@ -1,12 +1,16 @@
 package be.howest.twentytwo.parametergame.model.component;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
 /**
  * Sprite / Texture data
  */
 public class SpriteComponent implements Component, Poolable {
+	
+	public static final ComponentMapper<SpriteComponent> MAPPER = ComponentMapper.getFor(SpriteComponent.class);
+
 	// TODO: TextureRegion / Texture? Probably TextureRegion if we're doing spritesheets
 	
 	@Override
