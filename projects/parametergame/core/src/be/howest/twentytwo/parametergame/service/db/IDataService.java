34f98101997e253fe87eba0ab5c7d75d405742ee
Nick Dewitte <Nick Dewitte>
2016-11-10 21:47:54 +0100

    uploading faulty code to fix merge issue

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/db/IDataService.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/db/IDataService.java
@@ -5,10 +5,20 @@ import java.util.Map;
 public interface IDataService {
 	//This is the general class, that communicates with different classes, such as MySQLDataService, T-SQLDataService,....
 
-	public Map<String, String> getEnemy(String name);
+	public LevelData getLevel(String key);
 	
-	public Map<String, String> getShip(String name);
+	public UserData getUser(String serverID);
 	
-	public Map<String, String> getDrone(String name);
+	public Collection<EnemyData> getEnemies(String... name);	//allows you to write getEnemies("Str1","Str2) instead of getEnemies(Str[])
+	
+	public Collection<ShipData> getShips(UserData user);
+	
+	public Collection<DroneData> getDrones(UserData user);
+	
+	public void saveUser(UserData);
+	
+	public void saveShip(ShipData);
+	
+	public void saveDrone(DroneData);
 	
 }
