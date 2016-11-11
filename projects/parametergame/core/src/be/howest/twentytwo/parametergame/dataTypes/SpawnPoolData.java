5201f902d8298d0a1198bb607386db0ed68d53a2
Nick Dewitte <Nick Dewitte>
2016-11-11 18:38:24 +0100

    pushing so kevin can get a look

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/SpawnPoolData.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/SpawnPoolData.java
@@ -1,5 +1,16 @@
 package be.howest.twentytwo.parametergame.dataTypes;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 public class SpawnPoolData {
+	
+	private List<ClusterData> clusters;	//Put one of each kind of cluster in here, with amount X. If X is decreased, check the value. if it reaches 0, remove from here, and also from chances. recalculate total chance
+	private Map<ClusterData, Float > chances;	//keeps track of each clusters chance to get elected
+	
+	public ClusterData getRandomCluster() {
+		
+	}
 
 }
