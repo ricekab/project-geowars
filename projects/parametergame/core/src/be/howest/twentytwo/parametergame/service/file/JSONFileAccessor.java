e5037401f3141bacc04bb9ca1cb0e02b883d8d88
Nick Dewitte <Nick Dewitte>
2016-11-16 19:09:44 +0100

    testing git ignore modification

---
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/file/JSONFileAccessor.java
@@ -0,0 +1,13 @@
+package be.howest.twentytwo.parametergame.service.file;
+
+import be.howest.twentytwo.parametergame.dataTypes.LevelData;
+
+public class JSONFileAccessor implements IFileAccessor{
+
+	@Override
+	public LevelData getLevel(String location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
