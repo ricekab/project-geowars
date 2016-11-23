3bde007542cc5fa5d63e4543abb693de06405d39
Nick Dewitte <Nick Dewitte>
2016-11-23 14:38:22 +0100

    updated shipdata class&interface

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/ShipDataI.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/ShipDataI.java
@@ -1,11 +1,12 @@
 package be.howest.twentytwo.parametergame.dataTypes;
 
-import java.util.Collection;
+import java.util.Set;
 
 import com.badlogic.gdx.math.Vector2;
 
 public interface ShipDataI {
 	
-	public Collection<Vector2> getWeaponPositions();	//ship has multiple positions weapons can be fired from
-
+	public Set<Vector2> getWeaponPositions();	//ship has multiple positions weapons can be fired from
+	public void addWeaponPosition(Vector2 pos);
+	
 }
