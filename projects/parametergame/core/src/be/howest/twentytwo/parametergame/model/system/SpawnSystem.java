c2fcc6bda9aa317b133b22457b092c1cf0aa8fb5
kevincytang <kevincytang@hotmail.com>
2016-12-12 15:32:03 +0100

    Cleanup

---
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/model/system/SpawnSystem.java
@@ -0,0 +1,19 @@
+package be.howest.twentytwo.parametergame.model.system;
+
+import com.badlogic.ashley.systems.IntervalSystem;
+
+public class SpawnSystem extends IntervalSystem{
+	
+	public static final int PRIORITY = 1;
+
+	public SpawnSystem(float interval) {
+		super(interval, PRIORITY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void updateInterval() {
+		// TODO: Process spawn messages
+	}
+
+}
