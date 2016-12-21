23fa8253e59e97cac60a464562af81d105697a43
Nick Dewitte <Nick Dewitte>
2016-12-21 15:52:47 +0100

    updated the playerShipData class and it's interface

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/PlayerShipDataI.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/PlayerShipDataI.java
@@ -4,7 +4,31 @@ import java.util.Collection;
 
 public interface PlayerShipDataI {
 
-	public ShipDataI getShipData();
 	
+	
+	/*private String id;
+	private float mass;
+	private int exp;
+	private int lvl;
+	private float geomRadius;*/
+
+	public void setShipData(ShipDataI ship);
+	public void setDrones(Collection<DroneDataI> drones);
+	
+	public void setId(String id);
+	public void setMass(float mass);
+	public void setExp(int exp);
+	public void setLvl(int lvl);
+	public void setGeomRadius(float radius);
+
+	
+	public ShipDataI getShipData();
 	public Collection<DroneDataI> getDrones();
+	public void addDrone(DroneDataI drone);
+	
+	public String getId();
+	public float getMass();
+	public int getExp();
+	public int getLvl();
+	public float getGeomRadius();
 }
