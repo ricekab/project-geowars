006cc7df99163a669c434431bbb1674b464ff891
Nick Dewitte <Nick Dewitte>
2016-12-22 16:49:15 +0100

    added testing package

---
+++ projects/parametergame/core/src/jUnit/SQLDataServiceTests.java
@@ -0,0 +1,14 @@
+package jUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SQLDataServiceTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
