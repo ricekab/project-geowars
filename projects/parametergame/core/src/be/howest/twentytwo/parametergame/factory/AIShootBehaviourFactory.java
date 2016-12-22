74858065a8e98734d494504fbf8f64266f4a1a10
WardVandenBerghe <ward_vandenberghe@hotmail.com>
2016-12-22 23:05:31 +0100

    As requested

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/factory/AIShootBehaviourFactory.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/factory/AIShootBehaviourFactory.java
@@ -1,8 +1,8 @@
 package be.howest.twentytwo.parametergame.factory;
 
+import be.howest.twentytwo.parametergame.model.ai.BasicAIShootBehaviour;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import be.howest.twentytwo.parametergame.model.ai.IAIShootBehaviour;
 import be.howest.twentytwo.parametergame.model.ai.NullAIShootBehaviour;
@@ -22,15 +22,26 @@ public class AIShootBehaviourFactory {
 			return map.get(behaviour);
 		}
 		switch (behaviour){
-			case "ggwp":
-				// Add cases here, for each case:
-				// create new behaviour for string
-				// add behaviour to the map
-				// return the behaviour
-				default:
-					Gdx.app.error("IAIMoveFactory", "ERR: Could not produce behaviour, returned NullBehaviour instead");
-				
-					return new NullAIShootBehaviour();
+			case "Scouter":
+                            // Add cases here, for each case:
+                            // create new behaviour for string
+                            // add behaviour to the map
+                            // return the behaviour
+                            IAIShootBehaviour scouter = new BasicAIShootBehaviour(60, 80);
+                            map.put(behaviour, scouter);
+                            return scouter;
+                        case "Brutalizer":
+                            IAIShootBehaviour brutalizer = new BasicAIShootBehaviour(120, 50);
+                            map.put(behaviour, brutalizer);
+                            return brutalizer;
+                        case "Drone":
+                            IAIShootBehaviour drone = new BasicAIShootBehaviour(60, 70);
+                            map.put(behaviour, drone);
+                            return drone;
+                        default:
+                            Gdx.app.error("IAIMoveFactory", "ERR: Could not produce behaviour, returned NullBehaviour instead");
+
+                            return new NullAIShootBehaviour();
 		}
 	}
 }
