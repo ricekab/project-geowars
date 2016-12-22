4d7a9dd0a5e4035412a113d20827e1f0303f62a5
Nick Dewitte <Nick Dewitte>
2016-12-22 17:57:42 +0100

    My first test!

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/db/Writer.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/service/db/Writer.java
@@ -8,9 +8,11 @@ public class Writer {
 	public static void main(String[] args) {
 		
 		SQLDataService db = SQLDataService.getInstance();
-		UserData user = new UserData("The_Legend_27","123457","Advanced JS difficulty");
+		UserData user = new UserData("The_Legend_27","12345678","Advanced JS difficulty");
 
 		db.saveUser(user);
+		
+		System.out.println(db.getUser("The_Legend_27").getPasswordHashed());
 		
 		
 		
--- projects/parametergame/core/src/jUnit/SQLDataServiceTests.java
+++ projects/parametergame/core/src/jUnit/SQLDataServiceTests.java
@@ -2,13 +2,26 @@ package jUnit;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import be.howest.twentytwo.parametergame.dataTypes.UserDataI;
+import be.howest.twentytwo.parametergame.service.db.SQLDataService;
+
 public class SQLDataServiceTests {
+	
+	private SQLDataService db;
+	
+	@Before
+	public void init() {
+		db = SQLDataService.getInstance();
+	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		String desiredUsername = "The_Legend_27";
+		UserDataI user = db.getUser(desiredUsername);
+		assertTrue("success!", user.getUser().equals(desiredUsername));
 	}
 
 }
