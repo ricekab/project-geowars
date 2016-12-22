0e97b19102babdc0cd634b2dad924022ff109f31
Nick Dewitte <Nick Dewitte>
2016-12-22 11:17:43 +0100

    added powerupData

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/PowerupData.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/PowerupData.java
@@ -1,5 +1,47 @@
 package be.howest.twentytwo.parametergame.dataTypes;
 
-public class PowerupData {
+public class PowerupData implements PowerupDataI {
+	
+	private String powerupId;
+	private String effectId;
+	private int powerupDuration;
+	private int powerupLifetime;
+	private String effectTpe;
+	private int effectStrength;
+	
+	public PowerupData(String PowerupId, String effectId, int powerupDuration, int powerupLifetime, String effectType, int effectStrength) {
+		this.powerupId = powerupId;
+		this.effectId = effectId;
+		this.powerupDuration = powerupDuration;
+		this.powerupLifetime = powerupLifetime;
+		this.effectTpe = effectType;
+		this.effectStrength = effectStrength;
+	}
+	
+	//	GETTERS
+
+	public String getPowerupId() {
+		return powerupId;
+	}
+
+	public String getEffectId() {
+		return effectId;
+	}
+
+	public int getPowerupDuration() {
+		return powerupDuration;
+	}
+
+	public int getPowerupLifetime() {
+		return powerupLifetime;
+	}
+
+	public String getEffectTpe() {
+		return effectTpe;
+	}
+
+	public int getEffectStrength() {
+		return effectStrength;
+	}
 
 }
--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/PowerupDataI.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/PowerupDataI.java
@@ -2,4 +2,11 @@ package be.howest.twentytwo.parametergame.dataTypes;
 
 public interface PowerupDataI {
 
+	public String getPowerupId();
+	public String getEffectId();
+	public int getPowerupDuration();
+	public int getPowerupLifetime();
+	public String getEffectTpe();
+	public int getEffectStrength();
+	
 }
