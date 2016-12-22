d6a8a11dc5844cfb8d5d3d67575c65b94cd49593
Nick Dewitte <Nick Dewitte>
2016-12-22 21:34:30 +0100

    interface method for kev

--- projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/SpawnPoolDataI.java
+++ projects/parametergame/core/src/be/howest/twentytwo/parametergame/dataTypes/SpawnPoolDataI.java
@@ -1,8 +1,11 @@
 package be.howest.twentytwo.parametergame.dataTypes;
 
+import java.util.Collection;
+
 public interface SpawnPoolDataI {
 	
 	public ClusterDataI getRandomCluster();
+	public Collection<ClusterDataI> getAllClusters();
 	public float getSpawnTreshold();
 	public float getSpawnTresholdIncrease();
 	
